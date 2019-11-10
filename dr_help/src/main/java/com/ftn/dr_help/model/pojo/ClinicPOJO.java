41bd1145bdb5cab992573d7c7facdc0b8105410d
tiberius <tiberlas1997@gmail.com>
2019-11-10 21:04:57 +0100

    test2

--- dr_help/src/main/java/com/ftn/dr_help/model/pojo/ClinicPOJO.java
+++ dr_help/src/main/java/com/ftn/dr_help/model/pojo/ClinicPOJO.java
@@ -3,5 +3,15 @@ package com.ftn.dr_help.model.pojo;
 public class ClinicPOJO {
 
 	private String clinic;
+
+	public String getClinic() {
+		return clinic;
+	}
+
+	public void setClinic(String clinic) {
+		this.clinic = clinic;
+	}
+	
+	
 	
 }
