d0562b487fd316459e67a016993de269506d7809
rsnikola <rsnikola@yahoo.com>
2019-11-10 21:09:06 +0100

    test2-conflictResolution

--- dr_help/src/main/java/com/ftn/dr_help/model/pojo/ClinicPOJO.java
+++ dr_help/src/main/java/com/ftn/dr_help/model/pojo/ClinicPOJO.java
@@ -4,6 +4,14 @@ public class ClinicPOJO {
 
 	private String clinic;
 	private String address;
-	
+
+
+	public String getClinic() {
+		return clinic;
+	}
+
+	public void setClinic(String clinic) {
+		this.clinic = clinic;
+	}	
 	
 }
