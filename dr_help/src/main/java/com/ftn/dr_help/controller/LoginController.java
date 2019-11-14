43535a42c3a8b9a8df3f3a4b73bc0c6ca90e92bf
rsnikola <rsnikola@yahoo.com>
2019-11-15 00:30:44 +0100

    Login dtos added

--- dr_help/src/main/java/com/ftn/dr_help/controller/LoginController.java
+++ dr_help/src/main/java/com/ftn/dr_help/controller/LoginController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.ftn.dr_help.dto.LoginRequestDTO;
 import com.ftn.dr_help.dto.LoginResponseDTO;
 import com.ftn.dr_help.model.enums.RoleEnum;
-
+ 
 @RestController
 @RequestMapping (value = "/api")
 @CrossOrigin (origins="http://localhost:3000")
