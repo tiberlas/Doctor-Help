304a3276a1a774f196a43a6d7f382958d46141dc
rsnikola <rsnikola@yahoo.com>
2019-11-15 00:18:43 +0100

    Login front done

---
+++ dr_help/src/main/java/com/ftn/dr_help/controller/LoginController.java
@@ -0,0 +1,34 @@
+package com.ftn.dr_help.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.ftn.dr_help.dto.LoginRequestDTO;
+import com.ftn.dr_help.dto.LoginResponseDTO;
+import com.ftn.dr_help.model.enums.RoleEnum;
+
+@RestController
+@RequestMapping (value = "/api")
+@CrossOrigin (origins="http://localhost:3000")
+public class LoginController {
+	
+	@PostMapping (value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<LoginResponseDTO> login (@RequestBody  LoginRequestDTO loginRequest){
+
+		System.out.println("Email: " + loginRequest.getEmail());
+		System.out.println("Password: " + loginRequest.getPassword());
+		
+		LoginResponseDTO retVal = new LoginResponseDTO ();
+		retVal.setUserRole(RoleEnum.DOCTOR);
+		
+		return new ResponseEntity<LoginResponseDTO> (retVal, HttpStatus.OK);
+	}
+	
+	
+}
---
+++ react/drhelp_react/src/LoginPage.js
@@ -0,0 +1,116 @@
+import React from 'react';
+import Button from 'react-bootstrap/Button'
+import Form from 'react-bootstrap/Form';
+import FormControl from 'react-bootstrap/FormControl';
+import TempHome from './TempHome.js';
+import axios from 'axios';
+
+
+//import { createAppContainer } from 'react-navigation';
+//import { createStackNavigator } from 'react-navigation-stack';
+
+
+class LoginPage extends React.Component {
+
+	state = {
+    	loggedIn: false, 
+  	}
+
+	
+	render () {
+
+		if (this.state.loggedIn === true) {
+			let email = document.getElementById('tb_email');
+			let password = document.getElementById('tb_password');
+			//email.value
+			
+			let userRole = "guest";
+
+			if ((email.value === 'doctor') && (password.value === 'doctor')) {
+				userRole = 'doctor';
+			} 
+			else if ((email.value === 'nurse') && (password.value === 'nurse')) {
+				userRole = 'nurse';
+			}
+			else if ((email.value === 'clinicAdmin') && (password.value === 'clinicAdmin')) {
+				userRole = 'clinicAdmin';
+			}
+			else if ((email.value === 'centreAdmin') && (password.value === 'centreAdmin')) {
+				userRole = 'centreAdmin';
+			}
+			else if ((email.value === 'patient') && (password.value === 'patient')) {
+				userRole = 'patient';
+			}
+
+			if (email.value.length > 3) {
+				if  (password.value.length > 3) {
+
+					axios.post ('http://localhost:8080/api/login', {		
+						email: email.value, 
+						password: password.value
+					}).then (function (response) {
+						alert (response.);
+						if (response.userRole !== 'guest') {
+							userRole = response.userRole;
+						}
+					});
+
+					if (userRole !== 'guest') {
+
+
+						return (
+							<div>
+								<TempHome userRole={userRole}/>			
+							</div>
+						)
+					}
+					else {
+						alert ('Invalid user id');
+						this.setState (() => ({
+							loggedIn: false
+						}))
+					}
+				}
+				else {
+					alert ('Password too short');
+					this.setState (() => ({
+						loggedIn: false
+					}))
+				}
+			}
+			else  {
+				alert ('Username too short');
+				this.setState (() => ({
+						loggedIn: false
+				}))
+			}
+		}
+
+		const handleLogIn = () => {
+			this.setState (() => ({
+				loggedIn: true
+			}))
+		}
+
+
+		return (
+			<div>
+				<Form>
+					<FormControl type="text" placeholder="Email" id="tb_email"/>
+					<FormControl type="password" placeholder="Password" id='tb_password'/>
+					<Button 	
+						variant="primary" 
+						type="submit" 
+						onClick={handleLogIn}
+					>
+						Submit
+					</Button>
+				</Form>
+			</div>
+		)
+	}
+
+}
+
+
+export default LoginPage
