6c17bf1444a5bda923a64348a6f3287e0ac2658f
Dusan <nikolic.dusan.dey@gmail.com>
2019-12-12 23:12:39 +0100

    fixed headers

--- react/drhelp_react/src/components/nurse/NurseHeader.jsx
+++ react/drhelp_react/src/components/nurse/NurseHeader.jsx
@@ -10,19 +10,33 @@ class NurseHeader extends Component {
     render() { 
         return ( 
             <Navbar bg="light" expand="lg">
-            <Navbar.Brand >{this.context.nurse.firstName}&nbsp;{this.context.nurse.lastName}</Navbar.Brand>
+            <Navbar.Brand >
+              <Nav> 
+                  <Nav.Link>
+                    <Link exact to = '/home' class="nav-link">
+                      <strong> drHelp </strong>
+                    </Link>
+                  </Nav.Link>
+                </Nav>
+            </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                 <Nav className="mr-auto">
-                    <Nav.Link >
-                        <Link exact to = '/nurse/profile' class="nav-link">Profile</Link>
-                    </Nav.Link>
+                    
 
                     <Nav.Link>
                             <Link exact to='/nurse/patient-list' class="nav-link">Patient&nbsp;list</Link>
                     </Nav.Link>
                 </Nav>
                 <Nav className="justify-content-end" style={{ width: "100%" }}>
+                    
+
+                    <Nav.Link>
+                    <Link exact to = '/nurse/profile' class="nav-link">
+                          {this.context.nurse.firstName}&nbsp;{this.context.nurse.lastName}
+                    </Link>
+                    </Nav.Link>
+
                     <Nav.Link>
                         <Link exact to='/login' onClick={this.props.logout} class="nav-link">Logout</Link>
                     </Nav.Link>
