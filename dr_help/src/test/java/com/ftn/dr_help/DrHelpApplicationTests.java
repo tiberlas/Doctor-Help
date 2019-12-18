a2ae4546aaa0269b901cf20bac9a235d9bca5700
tiberius <tiberlas1997@gmail.com>
2019-12-18 20:22:45 +0100

    clean up

--- dr_help/src/test/java/com/ftn/dr_help/DrHelpApplicationTests.java
+++ dr_help/src/test/java/com/ftn/dr_help/DrHelpApplicationTests.java
@@ -1,12 +1,29 @@
 package com.ftn.dr_help;
 
-import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-//@SpringBootTest
+import com.ftn.dr_help.model.pojo.NursePOJO;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
 class DrHelpApplicationTests {
-	
+
+	@Test
+	void shouldNotPass() {
+		NursePOJO nurse = new NursePOJO();
+		nurse.setFirstName("ANA");
+
+		assertEquals("EVA", nurse.getFirstName());
+	}
+
 	@Test
 	void shouldPass() {
+		NursePOJO nurse = new NursePOJO();
+		nurse.setFirstName("ANA");
+
+		assertEquals("ANA", nurse.getFirstName());
 	}
 	
 }
