dd2af8e00162a7a2300b0aaccdae58a4b98b0b78
rsnikola <rsnikola@yahoo.com>
2020-02-05 11:39:55 +0100

    Test get clinic list

--- dr_help/src/test/java/com/ftn/dr_help/service/DoctorServiceTest.java
+++ dr_help/src/test/java/com/ftn/dr_help/service/DoctorServiceTest.java
@@ -11,7 +11,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.junit.Before;
-import org.junit.Test; 
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mockito;
