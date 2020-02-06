8111298ce38d125e8fba60211f2d178a6b24c91a
tiberius <tiberlas1997@gmail.com>
2020-02-06 19:30:31 +0100

    added test for doctor repository

--- dr_help/src/test/java/com/ftn/dr_help/repository/LeaveRequestRepositoryTest.java
+++ dr_help/src/test/java/com/ftn/dr_help/repository/LeaveRequestRepositoryTest.java
@@ -5,6 +5,7 @@ import static org.junit.Assert.assertEquals;
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
