14a1853ec701310b8ba10a812830e69cace2c2bd
tiberius <tiberlas1997@gmail.com>
2020-02-07 16:03:11 +0100

    work in progress

--- dr_help/src/test/java/com/ftn/dr_help/controller/RoomControllerTest.java
+++ dr_help/src/test/java/com/ftn/dr_help/controller/RoomControllerTest.java
@@ -1,7 +1,17 @@
 package com.ftn.dr_help.controller;
 
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.fail;
 import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.annotation.PostConstruct;
@@ -9,6 +19,7 @@ import javax.annotation.PostConstruct;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
@@ -24,8 +35,10 @@ import org.springframework.web.context.WebApplicationContext;
 import com.ftn.dr_help.dto.LoginRequestDTO;
 import com.ftn.dr_help.dto.LoginResponseDTO;
 import com.ftn.dr_help.dto.RoomDTO;
+import com.ftn.dr_help.dto.RoomSearchDTO;
 import com.ftn.dr_help.service.RoomService;
 
+import constants.RoomsConstants;
 import constants.UserConstants;
 
 @RunWith(SpringRunner.class)
@@ -57,8 +70,6 @@ public class RoomControllerTest {
                 .webAppContextSetup(webApplicationContext)
                 .apply(springSecurity())
                 .build();
-        
-        
     }
 	
 	@Before
@@ -68,11 +79,106 @@ public class RoomControllerTest {
 						new LoginRequestDTO(UserConstants.CLINIC_ADMIN_MAIL, UserConstants.CLINIC_ADMIN_PASSWORD), 
 						LoginResponseDTO.class);
 		accessToken = "Bearer "+responseEntity.getBody().getJwtToken();
+		
+		RoomDTO room = new RoomDTO(
+				RoomsConstants.ID,
+				RoomsConstants.NAME,
+				RoomsConstants.NUMBER,
+				RoomsConstants.PROCEDURE_NAME,
+				RoomsConstants.PROCEDURE_ID,
+				RoomsConstants.RESERVED,
+				RoomsConstants.FIRST_FREE);
+		
+		roomList = new ArrayList<>();
+		roomList.add(room);
 	}
 	
 	@Test
-	public void test() {
+	public void testAll() {
+		Mockito.when(roomService.findAll(Mockito.any(String.class))).thenReturn(roomList);
 		
+		try {
+			
+			this.mockMvc.perform(get(RoomsConstants.ALL_URL)
+					.contentType(contentType)
+					.header("Authorization", accessToken))
+			.andDo(print())
+			.andExpect(status()
+					.isOk())
+			.andExpect(content().contentType(contentType))
+	        .andExpect(jsonPath("$", hasSize(1)))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(RoomsConstants.ID.intValue())));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@Test
+	public void testOne() {
+		Mockito.when(roomService.findOne(Mockito.any(Long.class), Mockito.any(String.class))).thenReturn(roomList.get(0));
+		
+		try {
+			
+			this.mockMvc.perform(get(RoomsConstants.ONE_URL)
+					.contentType(contentType)
+					.header("Authorization", accessToken))
+			.andDo(print())
+			.andExpect(status()
+					.isOk())
+			.andExpect(content().contentType(contentType))
+            .andExpect(jsonPath("$.id").value(RoomsConstants.ID.intValue()));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@Test
+	public void testOneFail() {
+		Mockito.when(roomService.findOne(Mockito.any(Long.class), Mockito.any(String.class))).thenReturn(roomList.get(0));
+		
+		try {
+			
+			this.mockMvc.perform(get(RoomsConstants.ONE_NOT_URL)
+					.contentType(contentType)
+					.header("Authorization", accessToken))
+			.andDo(print())
+			.andExpect(status()
+					.isNotFound());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@Test
+	public void testSearch() {
+		Mockito.when(roomService.search(Mockito.any(RoomSearchDTO.class), Mockito.any(String.class))).thenReturn(roomList);
+		
+		try {
+			RoomSearchDTO room = new RoomSearchDTO(
+								RoomsConstants.NAME, 
+								new Long(RoomsConstants.NUMBER), 
+								RoomsConstants.PROCEDURE_ID, 
+								RoomsConstants.FIRST_FREE);
+			
+			String json = TestUtil.json(room)
+			this.mockMvc.perform(post(RoomsConstants.SEARCH_URL)			
+					.contentType(contentType)
+					.content(json)
+					.header("Authorization", accessToken))
+			.andDo(print())
+			.andExpect(status()
+					.isNotFound());
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
 	}
 
 }
