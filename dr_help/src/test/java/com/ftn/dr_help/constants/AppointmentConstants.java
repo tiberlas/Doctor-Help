81fca7c64dad251bfe5523b624a7723d43a60042
Dusan <nikolic.dusan.dey@gmail.com>
2020-02-07 15:33:20 +0100

    merged controller tests

--- dr_help/src/test/java/com/ftn/dr_help/constants/AppointmentConstants.java
+++ dr_help/src/test/java/com/ftn/dr_help/constants/AppointmentConstants.java
@@ -3,13 +3,9 @@ package com.ftn.dr_help.constants;
 public class AppointmentConstants {
 
 	public static final String DOCTOR_ID = "1";
-	
 	public static final String CLINIC_ID = "1";
-	
 	public static final String PROCEDURE_TYPE_ID = "1";
-	
 	public static final String FILTER_DATE = "28.03.2020 10:00";
-	
 	public static final Integer RESULTS_MATCH = 1;
 	
 	public static final String DOCTOR_NAME = "Pera Peric";
