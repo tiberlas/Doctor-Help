d9f919921c35ff224bfa322d8c7d547b6d89606b
Dusan <nikolic.dusan.dey@gmail.com>
2020-02-07 16:03:22 +0100

    added appointment reservation integration tests

--- dr_help/src/test/java/com/ftn/dr_help/constants/LoginConstants.java
+++ dr_help/src/test/java/com/ftn/dr_help/constants/LoginConstants.java
@@ -3,7 +3,6 @@ package com.ftn.dr_help.constants;
 public class LoginConstants {
 
 	public static final String PATIENT_USERNAME = "happymeal@gmail.com";
-	
 	public static final String PATIENT_PASSWORD = "whoppa42";
 	
 	
