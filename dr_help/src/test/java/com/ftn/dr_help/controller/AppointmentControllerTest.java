58953e033b6d50b47c38a715983bd53108f68ab7
tiberius <tiberlas1997@gmail.com>
2020-02-07 12:31:10 +0100

    appointment controller tests

--- dr_help/src/test/java/com/ftn/dr_help/controller/AppointmentControllerTest.java
+++ dr_help/src/test/java/com/ftn/dr_help/controller/AppointmentControllerTest.java
@@ -1,16 +1,14 @@
 package com.ftn.dr_help.controller;
 
-import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.fail;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
-import java.nio.charset.Charset;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.hamcrest.Matchers.hasItem;
+import static org.hamcrest.Matchers.hasSize;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -20,22 +18,19 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
+import org.mockito.internal.hamcrest.HamcrestArgumentMatcher;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
-import static org.springframework.security.test.web.servlet.setup.SecurityMockMvcConfigurers.springSecurity;
+
 import com.ftn.dr_help.dto.LoginRequestDTO;
 import com.ftn.dr_help.dto.LoginResponseDTO;
 import com.ftn.dr_help.dto.RequestingAppointmentDTO;
@@ -46,7 +41,7 @@ import constants.UserConstants;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest(webEnvironment=WebEnvironment.RANDOM_PORT)
-public class AppointmentControllerGetPredefinedTest {
+public class AppointmentControllerTest {
 
 	@Autowired
     private TestRestTemplate restTemplate;
@@ -65,8 +60,7 @@ public class AppointmentControllerGetPredefinedTest {
 	
 	private MediaType contentType = new MediaType(
 			MediaType.APPLICATION_JSON.getType(),
-            MediaType.APPLICATION_JSON.getSubtype(),
-            Charset.forName("utf8"));
+            MediaType.APPLICATION_JSON.getSubtype());
 	
 	@PostConstruct
     public void setUp() {
@@ -77,7 +71,7 @@ public class AppointmentControllerGetPredefinedTest {
     }
 	
 	@Before
-	public void login() {
+	public void login() throws Exception {
 		ResponseEntity<LoginResponseDTO> responseEntity = 
 				restTemplate.postForEntity(UserConstants.LOGIN_URL, 
 						new LoginRequestDTO(UserConstants.CLINIC_ADMIN_MAIL, UserConstants.CLINIC_ADMIN_PASSWORD), 
@@ -86,14 +80,14 @@ public class AppointmentControllerGetPredefinedTest {
 		
 		
 		RequestingAppointmentDTO newAppointemnt = new RequestingAppointmentDTO(
-				1L, 
-				"01/25/2020 10:30 AM", 
-				"transplatacija jetre", 
-				"dr Bozidar", 
-				"sestra Milunka", 
-				"Kunic", 
-				25L, 
-				"02:00");
+				AppointmentConstants.APPOINTMENT_ID, 
+				AppointmentConstants.DATE, 
+				AppointmentConstants.TYPE, 
+				AppointmentConstants.DOCTOR_NAME, 
+				AppointmentConstants.NURSE_NAME, 
+				AppointmentConstants.PATIENT_NAME, 
+				AppointmentConstants.TYPE_ID, 
+				AppointmentConstants.DURATION);
 		requests.add(newAppointemnt);
 	}
 	
@@ -101,7 +95,7 @@ public class AppointmentControllerGetPredefinedTest {
 	public void smokeTestNotFound() {
 		try {
 
-			mockMvc.perform(get("api/appointments/requests/all")
+			mockMvc.perform(get(AppointmentConstants.REQUEST_ONE_NOT_URL)
 					.contentType(contentType)
 					.header("Authorization", accessToken))
 				    .andExpect(status().isNotFound());
@@ -121,7 +115,7 @@ public class AppointmentControllerGetPredefinedTest {
 							Mockito.any(String.class))
 					).thenReturn(requests);
 			
-			mockMvc.perform(get("/api/appointments/requests/all")
+			mockMvc.perform(get(AppointmentConstants.REQUEST_ALL_URL)
 					.contentType(contentType)
 					.header("Authorization", accessToken))
 				    .andExpect(status().isOk());
@@ -133,32 +127,42 @@ public class AppointmentControllerGetPredefinedTest {
 	}
 	
 	@Test
-	public void test() {
-		Mockito.when(appointmentService.getAllRequests("test@mail")).thenReturn(requests);
+	public void testAll() throws Exception {
+		Mockito.when(appointmentService.getAllRequests(Mockito.any(String.class))).thenReturn(requests);
 		
 		try {
 			
-			this.mockMvc.perform(get("api/appointments/requests/all")
+			this.mockMvc.perform(get(AppointmentConstants.REQUEST_ALL_URL)
 					.contentType(contentType)
 					.header("Authorization", accessToken))
 			.andDo(print())
 			.andExpect(status()
 					.isOk())
 			.andExpect(content().contentType(contentType))
-			//.andExpect(jsonPath("$").value(hasSize(LIST_AVAILABLE_DOCTORS_COUNT)))
-            .andExpect(jsonPath("$.[*].id").value(1l))
-            .andExpect(jsonPath("$.[*].id").value(hasItem(NEW_DOCTOR_2_ID.intValue())));
-					
-//					andExpect(content().json("[{"
-//					+ "\"id\": 1"
-//					+ "\"date\": \"01/25/2020 10:30 AM\""
-//					+ "\"type\": \"transplatacija jetre\""
-//					+ "\"doctor\": \"dr Bozidar\""
-//					+ "\"nurse\": \"sestra Milunka\""
-//					+ "\"patient\": \"Kunic\""
-//					+ "\"typeId\": 25"
-//					+ "\"duration\": \"02:00\""
-//					+ "}]"));
+	        .andExpect(jsonPath("$", hasSize(1)))
+            .andExpect(jsonPath("$.[*].id").value(hasItem(AppointmentConstants.APPOINTMENT_ID.intValue())));
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			fail();
+		}
+	}
+	
+	@Test
+	public void testOne() throws Exception {
+		Mockito.when(appointmentService.getOneRequests(Mockito.any(Long.class))).thenReturn(requests.get(0));
+		
+		try {
+			
+			this.mockMvc.perform(get(AppointmentConstants.REQUEST_ONE_URL)
+					.contentType(contentType)
+					.header("Authorization", accessToken))
+			.andDo(print())
+			.andExpect(status()
+					.isOk())
+			.andExpect(content().contentType(contentType))
+
+            .andExpect(jsonPath("$.id").value(AppointmentConstants.APPOINTMENT_ID.intValue()));
 			
 		} catch (Exception e) {
 			e.printStackTrace();
